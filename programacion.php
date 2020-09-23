2e25a78799fb2e664d6281e14a12e50707a3f604
chopphead5001 <71047320+chopphead5001@users.noreply.github.com>
2020-09-23 15:24:02 -0300

    Add files via upload

---
+++ programacion.php
@@ -0,0 +1,20 @@
+//well i belive that we has a good feeling,
+so since right now i wanna said you that i´ll be abble to you if you wanna speak of wathever
+//how you has can see my english isn´t the best, but at least i´m triyng
+//if you think that your live have some value it´s fine, ´couse, why would your life make no sense? true?
+if everyone knows that...
+//maybe you´re wondering why, so let me give you one answer. 
+//the true question is: why not?, ´couse i´m free, true?, and everyone should have the chance to have a great live,
+and at where i´m going whit that´s shit, at the "no land beyond", what that´s mean?,
+nothing, it´s just a name of one weapon.
+//you was waiting something more deep?, i wondering what are you thinking?, are you even readind this?,
+you really care about something?, or you just are living normal days
+//what are you doing now?, today is a great day?, or it´s just other day like all the others days?,
+and if it´s one of thoses, will you change something?, or if it´s one great day, it´s diferent?,
+it´s will change something or just is a great day that stands out from the others,
+whe should measure the days of one relative way or one absolute way?
+//isn´t life is beautiful? i belive life is beautiful
+// from the heaven´s admission we wanted pay pardon for the problems on the human #23235564,
+he was fixed and now is living a great live, thank you for be waiting us,
+and follow enjoyng your stay on the earth #34.
+
